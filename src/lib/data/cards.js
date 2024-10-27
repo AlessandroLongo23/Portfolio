06f010c8e9d48152f7ab3c1825e3214b6d6a327d
Alessandro <longoa02@gmail.com>
2024-10-27 21:31:14 +0100

    QueensGame link added

--- src/lib/data/cards.js
+++ src/lib/data/cards.js
@@ -2,31 +2,31 @@ export let cards = [
     { 
         date: "2022 - present", 
         title: "Sapiens Notesharing",
-        href: "https://sapiens.alessandrolongo.com",
+        href: "",
         technologies: ['svelte', 'bootstrap', 'sass', 'mongodb', 'docker', 'vite', 'node'] 
     },
     { 
         date: "2018 - present", 
         title: "Tolom3.0", 
-        href: "https://sapiens.alessandrolongo.com",
+        href: "",
         technologies: ['svelte', 'tailwindcss', 'vite', 'node'] 
     },
     { 
         date: "2024", 
         title: "Computer Graphics Course", 
-        href: "https://sapiens.alessandrolongo.com",
+        href: "",
         technologies: ['svelte', 'tailwindcss', 'vite', 'node'] 
     },
     { 
         date: "2024", 
         title: "Pensieve", 
-        href: "https://sapiens.alessandrolongo.com",
+        href: "",
         technologies: ['tauri', 'svelte', 'tailwindcss', 'vite', 'node'] 
     },
     { 
         date: "2024", 
         title: "Queen's Game", 
-        href: "https://sapiens.alessandrolongo.com",
+        href: "https://queens-game.vercel.app/",
         technologies: ['tauri', 'svelte', 'tailwindcss', 'vite', 'node'] 
     },
 ];
